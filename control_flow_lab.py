f1211839728abbf56fd33446ce08c029894ff419
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-06-28 22:26:09 -0400

    wip: hw questions

---
+++ control_flow_lab.py
@@ -0,0 +1,54 @@
+# Write the code that:
+# 1. Calculates and prints the first 50 terms of the fibonacci sequence.
+
+print('hello')
+
+# 2. Print each term and number as follows:
+#      term: 0 / number: 0
+#      term: 1 / number: 1
+#      term: 2 / number: 1
+#      term: 3 / number: 2
+#      term: 4 / number: 3
+#      term: 5 / number: 5
+#      etc.
+
+# Hint: The next number is found by adding the two numbers before it
+
+# exercise-06 What's the  Season?
+
+# Write the code that:
+# 3. Prompts the user to enter the month (as three characters):
+#      Enter the month of the season (Jan - Dec):
+
+
+
+
+# 4. Then propts the user to enter the day of the month: 
+#      Enter the day of the month:
+
+
+
+
+
+
+# 5. Calculate what season it is based upon this chart:
+#      Dec 21 - Mar 19: Winter
+#      Mar 20 - Jun 20: Spring
+#      Jun 21 - Sep 21: Summer
+#      Sep 22 - Dec 20: Fall
+
+
+
+
+# 6. Print the result as follows:
+#      <Mmm> <dd> is in <season> 
+
+# Hints:
+# Consider using the in operator to check if a string is in a particular list/tuple like this:
+# if input_month in ('Jan', 'Feb', 'Mar'):
+
+
+
+
+# After setting the likely season, you can use another if...elif...else statement to "adjust" if
+# the day number falls within a certain range.
